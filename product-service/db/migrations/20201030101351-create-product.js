bfb63e70dd2c4a7dad097dedb0375beff2408a1a
joebui <dienbuijoe@outlook.com>
2020-10-30 21:43:51 +0700

    add index for product's category id

--- product-service/db/migrations/20201030101351-create-product.js
+++ product-service/db/migrations/20201030101351-create-product.js
@@ -1,30 +1,45 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("products", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      price: {
-        type: Sequelize.INTEGER,
-      },
-      categoryId: {
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable(
+        "products",
+        {
+          id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+          },
+          name: {
+            type: Sequelize.STRING,
+          },
+          price: {
+            type: Sequelize.INTEGER,
+          },
+          categoryId: {
+            type: Sequelize.INTEGER,
+          },
+          createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+          },
+          updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+          },
+        },
+        { transaction }
+      );
+      await queryInterface.addIndex("products", ["categoryId"], {
+        unique: false,
+        transaction,
+      });
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
   },
   down: async (queryInterface) => {
     await queryInterface.dropTable("products");
